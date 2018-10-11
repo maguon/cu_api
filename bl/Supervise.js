3b3312d978265da469ddb346b7f339ea91e03c4e
yangguoquan511 <guoquan.yang@myxxjs.com>
2018-10-11 10:31:50 +0800

    修改字段名字

---
+++ bl/Supervise.js
@@ -0,0 +1,181 @@
+'use strict';
+
+const sysMsg = require('../util/SystemMsg.js');
+const sysError = require('../util/SystemError.js');
+const encrypt = require('../util/Encrypt.js');
+const resUtil = require('../util/ResponseUtil.js');
+const listOfValue = require('../util/ListOfValue.js');
+const oAuthUtil = require('../util/OAuthUtil.js');
+const policeDao = require('../dao/PoliceInfoDAO.js');
+const serverLogger = require('../util/ServerLogger.js');
+const logger = serverLogger.createLogger('Police.js');
+const moment = require('moment/moment.js');
+
+const createPolice = (req,res,next) => {
+    let params = req.params;
+    new Promise((resolve,reject)=>{
+        policeDao.queryPolice({phone:params.phone},(error,rows)=>{
+            if (error) {
+                logger.error(' queryPolice ' + error.message);
+                resUtil.resetFailedRes(res,sysMsg.SYS_INTERNAL_ERROR_MSG) ;
+                return next();
+            } else {
+                if(rows && rows.length>0){
+                    logger.warn(' queryPolice ' +params.phone+ sysMsg.CUST_SIGNUP_REGISTERED);
+                    resUtil.resetFailedRes(res,sysMsg.CUST_SIGNUP_REGISTERED) ;
+                    return next();
+                }else{
+                    resolve();
+                }
+            }
+        })
+    }).then(()=>{
+        params.password = encrypt.encryptByMd5(params.password);
+        policeDao.createPolice(params,(error,result)=>{
+            if (error) {
+                logger.error(' createPolice ' + error.message);
+                throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            } else {
+                if(result && result.insertId>0){
+                    logger.info(' createPolice ' + 'success');
+                    let user = {
+                        userId : result.insertId,
+                        userStatus : listOfValue.USER_STATUS_ACTIVE
+                    }
+                    user.accessToken = oAuthUtil.createAccessToken(oAuthUtil.clientType.user,user.userId,user.userStatus);
+                    resUtil.resetQueryRes(res,user,null);
+                }else{
+                    logger.warn(' createPolice ' + 'false');
+                    resUtil.resetFailedRes(res,sysMsg.SYS_INTERNAL_ERROR_MSG);
+                }
+                return next();
+            }
+        })
+    })
+}
+const policeLogin = (req,res,next) =>{
+    let params = req.params;
+    policeDao.queryPolice({userName:params.userName},(error,rows)=>{
+        if (error) {
+            logger.error(' queryPolice ' + error.message);
+            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+        } else {
+            if(rows && rows.length<1){
+                logger.warn(' queryPolice ' +params.userName+ sysMsg.ADMIN_LOGIN_USER_UNREGISTERED);
+                resUtil.resetFailedRes(res,sysMsg.ADMIN_LOGIN_USER_UNREGISTERED) ;
+                return next();
+            }else{
+                let passwordMd5 = encrypt.encryptByMd5(params.password);
+                if(passwordMd5 != rows[0].password){
+                    logger.warn(' queryPolice ' +params.phone+ sysMsg.CUST_LOGIN_PSWD_ERROR);
+                    resUtil.resetFailedRes(res,sysMsg.CUST_LOGIN_PSWD_ERROR) ;
+                    return next();
+                }else{
+                    if(rows[0].status == listOfValue.ADMIN_USER_STATUS_NOT_ACTIVE){
+                        let user = {
+                            userId : rows[0].id,
+                            userStatus : rows[0].status
+                        }
+                        logger.info('queryPolice' +params.userName+ " not verified");
+                        resUtil.resetQueryRes(res,user,null);
+                        return next();
+                    }else{
+                        let user = {
+                            userId : rows[0].id,
+                            userStatus : rows[0].status
+                        }
+                        user.accessToken = oAuthUtil.createAccessToken(oAuthUtil.clientType.admin,user.userId,user.userStatus);
+                        logger.info('queryPolice' +params.userName+ " success");
+                        resUtil.resetQueryRes(res,user,null);
+                        return next();
+                    }
+                }
+            }
+        }
+    })
+}
+const queryPolice = (req,res,next) => {
+    let params = req.params;
+    policeDao.queryPolice(params,(error,rows)=>{
+        if (error) {
+            logger.error(' queryPolice ' + error.message);
+            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+        } else {
+            logger.info(' queryPolice ' + 'success');
+            resUtil.resetQueryRes(res,rows,null);
+            return next();
+        }
+    })
+}
+const getPoliceInfo = (req,res,next) => {
+    let params = req.params;
+    let myDate = new Date();
+    let strDate = moment(myDate).format('YYYYMMDD');
+    params.createdDateId = parseInt(strDate);
+    policeDao.getPoliceInfo(params,(error,rows)=>{
+        if (error) {
+            logger.error(' getPoliceInfo ' + error.message);
+            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+        } else {
+            logger.info(' getPoliceInfo ' + 'success');
+            resUtil.resetQueryRes(res,rows,null);
+            return next();
+        }
+    })
+}
+const updatePoliceInfo = (req,res,next) => {
+    let params = req.params;
+    policeDao.updateInfo(params,(error,result)=>{
+        if (error) {
+            logger.error(' updateInfo ' + error.message);
+            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+        } else {
+            logger.info(' updateInfo ' + 'success');
+            resUtil.resetUpdateRes(res,result,null);
+            return next();
+        }
+    })
+}
+const changePolicePassword = (req,res,next) => {
+    let params = req.params;
+    new Promise((resolve,reject) => {
+        policeDao.queryPolice(params,(error,rows)=>{
+            if (error) {
+                logger.error(' queryPolice ' + error.message);
+                throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            } else {
+                if(rows && rows.length<1){
+                    logger.warn(' queryPolice ' + sysMsg.ADMIN_LOGIN_USER_UNREGISTERED);
+                    resUtil.resetFailedRes(res,sysMsg.ADMIN_LOGIN_USER_UNREGISTERED);
+                    return next();
+                }else if(encrypt.encryptByMd5(params.originPassword) != rows[0].password){
+                    logger.warn(' queryPolice ' + sysMsg.CUST_ORIGIN_PSWD_ERROR);
+                    resUtil.resetFailedRes(res,sysMsg.CUST_ORIGIN_PSWD_ERROR);
+                    return next();
+                }else{
+                    resolve();
+                }
+            }
+        })
+    }).then(() => {
+        params.password = encrypt.encryptByMd5(params.newPassword);
+        policeDao.updatePassword(params,(error,result)=>{
+            if (error) {
+                logger.error(' updatePassword ' + error.message);
+                throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            } else {
+                logger.info(' updatePassword ' + 'success');
+                resUtil.resetUpdateRes(res,result,null);
+                return next();
+            }
+        })
+    })
+}
+module.exports = {
+    createPolice,
+    policeLogin,
+    queryPolice,
+    getPoliceInfo,
+    updatePoliceInfo,
+    changePolicePassword
+}
---
+++ dao/SuperviseDAO.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const db=require('../db/connection/MysqlDb.js');
+const serverLogger = require('../util/ServerLogger.js');
+const logger = serverLogger.createLogger('PoliceDAO.js');
+
+const createPolice = (params,callback) => {
+    let query = " insert into police_info (user_name,gender,password,status,phone) values ( ?,? , ? , ? , ?  )";
+    let paramsArray=[],i=0;
+    paramsArray[i++]=params.userName;
+    paramsArray[i++]=params.gender;
+    paramsArray[i++]=params.password;
+    paramsArray[i++]=params.status;
+    paramsArray[i]=params.phone;
+    db.dbQuery(query,paramsArray,(error,rows)=>{
+        logger.debug(' createPolice ');
+        return callback(error,rows);
+    });
+}
+const queryPolice = (params,callback) => {
+    let query = " select * from police_info where id is not null ";
+    let paramsArray=[],i=0;
+    if(params.policeId){
+        paramsArray[i++] = params.policeId;
+        query = query + " and id = ? ";
+    }
+    if(params.userName){
+        paramsArray[i++] = params.userName;
+        query = query + " and user_name = ? ";
+    }
+    if(params.phone){
+        paramsArray[i++] = params.phone;
+        query = query + " and phone = ? ";
+    }
+    db.dbQuery(query,paramsArray,(error,rows)=>{
+        logger.debug(' queryPolice ');
+        return callback(error,rows);
+    });
+}
+const getPoliceInfo = (params,callback) => {
+    let query = " select pi.user_name,pi.phone,pi.gender,date_format(pi.created_on,'%Y年%m月%d日') as date from police_info pi" +
+                " left join car_info ci on ci.police_id=pi.id" +
+                " where str_to_date(ci.created_on,'%Y-%m-%d') = ?";
+    let paramsArray=[],i=0;
+    paramsArray[i++]=params.createdDateId;
+    if(params.policeId){
+        query = query + " and pi.id = ? ";
+        paramsArray[i]=params.policeId;
+    }
+    db.dbQuery(query,paramsArray,(error,rows)=>{
+        logger.debug(' queryPoliceInfo ');
+        return callback(error,rows);
+    });
+}
+const updateInfo = (params,callback) => {
+    let query = " update police_info set gender = ? ,phone = ? where id = ?";
+    let paramsArray=[],i=0;
+    paramsArray[i++] = params.gender;
+    paramsArray[i++] = params.phone;
+    paramsArray[i] = params.policeId;
+    db.dbQuery(query,paramsArray,(error,rows)=>{
+        logger.debug(' updateInfo ');
+        return callback(error,rows);
+    });
+}
+const updatePassword = (params,callback) => {
+    let query = " update police_info set password = ? where id = ?";
+    let paramsArray=[],i=0;
+    paramsArray[i++] = params.password;
+    paramsArray[i] = params.policeId;
+    db.dbQuery(query,paramsArray,(error,rows)=>{
+        logger.debug(' updatePassword ');
+        return callback(error,rows);
+    });
+}
+module.exports = {
+    createPolice,
+    queryPolice,
+    getPoliceInfo,
+    updateInfo,
+    updatePassword
+}
