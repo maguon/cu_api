2f45c74d596e9dce04b98a50a7abfd50f6b26401
yangguoquan511 <guoquan.yang@myxxjs.com>
2018-11-13 12:26:01 +0800

    修改paymentDAO里的管理员查询

--- bl/Payment.js
+++ bl/Payment.js
@@ -112,7 +112,7 @@ const wechatPayment = (req,res,next)=>{
                 let resString = JSON.stringify(result);
                 let evalJson = eval('(' + resString + ')');
                 let myDate = new Date();
-                let myDateStr = myDate.getTime();
+                let myDateStr = myDate.toString();
                 let paySignMD5 = encrypt.encryptByMd5NoKey('appId='+sysConfig.wechatConfig.mpAppId+'&nonceStr='+evalJson.xml.nonce_str+'&package=prepay_id='+evalJson.xml.prepay_id+'&signType=MD5&timeStamp='+myDateStr+'&key=a7c5c6cd22d89a3eea6c739a1a3c74d1');
                 let paymentJson = [{
                     nonce_str: evalJson.xml.nonce_str,
