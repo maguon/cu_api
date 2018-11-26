fd78f37b425428a33a7b34ff67b8dc813a529e36
yangguoquan511 <guoquan.yang@myxxjs.com>
2018-11-26 15:35:53 +0800

    绑定车辆车牌和识别码查重

--- bl/UserCar.js
+++ bl/UserCar.js
@@ -34,26 +34,47 @@ const updateUserCar = (req,res,next)=>{
 };
 const addUserCar = (req,res,next)=>{
     let params = req.params;
-    userCarDao.addUserCar(params,(error,result)=>{
+    userCarDao.queryUserCar({vin:params.vin},(error,rows)=>{
         if(error){
-            logger.error('addUserCar' + error.message);
-            throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+            logger.error('queryUserCar' + error.message);
+            resUtil.resInternalError(error, res, next);
+        }else if(rows && rows.length > 0){
+            logger.warn('queryUserCar' + '该车辆识别码已经被绑定');
+            resUtil.resetFailedRes(res,'该车辆识别码已经被绑定',null);
         }else{
-            userCarDao.getUserCarNum(params,(error,rows)=>{
+            logger.info('queryUserCar'+'success');
+            userCarDao.queryUserCar({licensePlate:params.licensePlate},(error,rows)=>{
                 if(error){
-                    logger.error('getUserCarNum' + error.message);
-                    throw sysError.InternalError(error.message,sysMsg.SYS_INTERNAL_ERROR_MSG);
+                    logger.error('queryUserCar' + error.message);
+                    resUtil.resInternalError(error, res, next);
+                }else if(rows && rows.length > 0){
+                    logger.warn('queryUserCar' + '该车牌已经被绑定');
+                    resUtil.resetFailedRes(res,'该车牌已经被绑定',null);
                 }else{
-                    let num = rows[0].num + 1 ;
-                    params.num = num;
-                    userCarDao.updateUserCarNum(params,(error,result));
-                    logger.info('addUserCar' + 'success');
-                    resUtil.resetCreateRes(res,result,null);
-                    return next();
+                    userCarDao.addUserCar(params,(error,result)=>{
+                        if(error){
+                            logger.error('addUserCar' + error.message);
+                            resUtil.resInternalError(error, res, next);
+                        }else{
+                            userCarDao.getUserCarNum(params,(error,rows)=>{
+                                if(error){
+                                    logger.error('getUserCarNum' + error.message);
+                                    resUtil.resInternalError(error, res, next);
+                                }else{
+                                    let num = rows[0].num + 1 ;
+                                    params.num = num;
+                                    userCarDao.updateUserCarNum(params,(error,result));
+                                    logger.info('addUserCar' + 'success');
+                                    resUtil.resetCreateRes(res,result,null);
+                                    return next();
+                                }
+                            })
+                        }
+                    });
                 }
             })
         }
-    });
+    })
 };
 const delUserCar = (req,res,next)=>{
     let params = req.params;
