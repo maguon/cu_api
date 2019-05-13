018b57ec04f5cf9070c06900118d43c930594524
yingmei567 <yingmei.yan@myxxjs.com>
2019-05-13 10:55:51 +0800

    修改写入日志格式

--- bl/User.js
+++ bl/User.js
@@ -14,10 +14,10 @@ const updateUser = (req,res,next)=>{
     let params = req.params;
     userDao.updateUser(params,(error,result)=>{
         if(error){
-            logger.error('updateUser' + error.message);
+            logger.error('updateUser ' + error.message);
             resUtil.resInternalError(error, res, next);
         }else{
-            logger.info('updateUser' + 'success');
+            logger.info('updateUser ' + 'success');
             resUtil.resetUpdateRes(res,result,null);
             return next();
         }
@@ -28,28 +28,33 @@ const updatePassword=(req,res,next)=>{
     new Promise((resolve) => {
         userDao.queryUser(params,(error,rows)=>{
             if(error){
-                logger.error('updatePassword' + error.message);
+                logger.error('updatePassword queryUser ' + error.message);
                 resUtil.resInternalError(error, res, next);
-            }else if(rows && rows.length < 1){
-                logger.warn('updatePassword' + "尚未注册");
-                resUtil.resetFailedRes(res,"尚未注册");
-                return next();
-            }else if(encrypt.encryptByMd5(params.oldPassword) != rows[0].password){
-                logger.warn('updatePassword' + "原密码错");
-                resUtil.resetFailedRes(res,"原密码错误");
-                return next();
             }else{
-                resolve();
+                if(rows && rows.length < 1){
+                    logger.warn('updatePassword queryUser ' + "Have not yet registered.");
+                    resUtil.resetFailedRes(res,"尚未注册");
+                    return next();
+                }else{
+                    if(encrypt.encryptByMd5(params.oldPassword) != rows[0].password){
+                        logger.warn('updatePassword queryUser ' + "The original password is wrong.");
+                        resUtil.resetFailedRes(res,"原密码错误");
+                        return next();
+                    }else{
+                        logger.info('updatePassword queryUser ' + 'success');
+                        resolve();
+                    }
+                }
             }
         })
     }).then(() => {
         params.newPassword = encrypt.encryptByMd5(params.newPassword);
         userDao.updatePassword(params,(error,result)=>{
             if(error){
-                logger.error('updatePassword' + error.message);
+                logger.error('updatePassword user_update ' + error.message);
                 resUtil.resInternalError(error, res, next);
             }else{
-                logger.info('updatePassword' + 'success');
+                logger.info('updatePassword user_update ' + 'success');
                 resUtil.resetUpdateRes(res,result,null);
                 return next();
             }
@@ -60,7 +65,7 @@ const updateStatus=(req,res,next)=>{
     let params = req.params;
     userDao.updateStatus(params,(error,result)=>{
         if(error){
-            logger.error('updateStatus' + error.message);
+            logger.error('updateStatus ' + error.message);
             resUtil.resInternalError(error, res, next);
         }else{
             if(params.wechatStatus==1){
@@ -68,7 +73,7 @@ const updateStatus=(req,res,next)=>{
                 params.myDate = myDate;
                 userDao.updateCreatedTime(params,(error,result));
             }
-            logger.info('updateStatus' + 'success');
+            logger.info('updateStatus ' + 'success');
             resUtil.resetUpdateRes(res,result,null);
             return next();
         }
@@ -78,7 +83,7 @@ const updateType=(req,res,next)=>{
     let params = req.params;
     userDao.updateType(params,(error,result)=>{
         if(error){
-            logger.error('updateType' + error.message);
+            logger.error('updateType ' + error.message);
             resUtil.resInternalError(error, res, next);
         }else{
             if(params.authStatus==1){
@@ -86,7 +91,7 @@ const updateType=(req,res,next)=>{
                 params.myDate = myDate;
                 userDao.updateAuthTime(params,(error,result));
             }
-            logger.info('updateType' + 'success');
+            logger.info('updateType ' + 'success');
             resUtil.resetUpdateRes(res,result,null);
             return next();
         }
@@ -96,7 +101,7 @@ const updatePhone=(req,res,next)=>{
     let params = req.params;
     oauthUtil.getUserPhoneCode({phone:params.phone},(error,result)=>{
         if(error){
-            logger.error(' sendPswdSms ' + error.message);
+            logger.error('updatePhone getUserPhoneCode ' + error.message);
             resUtil.resetFailedRes(res,sysMsg.SYS_INTERNAL_ERROR_MSG);
             return next();
         }else{
@@ -106,16 +111,16 @@ const updatePhone=(req,res,next)=>{
                 params.authStatus = 1;
                 userDao.updatePhone(params,(error,result)=>{
                     if(error){
-                        logger.error('updatePhone' + error.message);
+                        logger.error('updatePhone user_update ' + error.message);
                         resUtil.resInternalError(error, res, next);
                     }else{
-                        logger.info('updatePhone' + 'success');
+                        logger.info('updatePhone user_update ' + 'success');
                         resUtil.resetUpdateRes(res,result,null);
                         return next();
                     }
                 })
             }else{
-                logger.warn('getSignCode' + '验证码错误');
+                logger.warn('updatePhone getUserPhoneCode ' + 'Verification code error.');
                 resUtil.resetFailedRes(res,'验证码错误',null);
                 return next();
             }
@@ -126,10 +131,10 @@ const queryUser = (req,res,next)=>{
     let params = req.params;
     userDao.queryUser(params,(error,result)=>{
         if(error){
-            logger.error('queryUser' + error.message);
+            logger.error('queryUser ' + error.message);
             resUtil.resInternalError(error, res, next);
         }else{
-            logger.info('queryUser' + 'success');
+            logger.info('queryUser ' + 'success');
             resUtil.resetQueryRes(res,result,null);
             return next();
         }
@@ -140,25 +145,28 @@ const userLogin = (req,res,next)=>{
     new Promise((resolve,reject)=>{
         userDao.queryUser({wechatId:params.wechatId},(error,rows)=>{
             if(error){
-                logger.error('queryUser'+error.message);
+                logger.error('userLogin queryUser '+error.message);
                 reject(error);
-            }else if(rows && rows.length < 1){
-                logger.info("userInfoRows"+rows[0]);
-                resolve(params);
             }else{
-                let user ={
-                    userId: rows[0].id,
-                    wechatName:rows[0].wechat_name,
-                    wechatId: rows[0].wechat_id,
-                    userStatus: rows[0].status
-                };
-                let myDate = new Date();
-                params.lastLoginOn = myDate;
-                user.lastLoginOn = params.lastLoginOn;
-                userDao.lastLoginOn({wechatId:params.wechatId,lastLoginOn:params.lastLoginOn},(error,rows));
-                user.accessToken = oauthUtil.createAccessToken(oauthUtil.clientType.user,user.userId,user.userStatus);
-                resUtil.resetQueryRes(res,user,null);
-                return next();
+                logger.info('userLogin queryUser ' + 'success');
+                if(rows && rows.length < 1){
+                    logger.info("userLogin userInfoRows "+rows[0]);
+                    resolve(params);
+                }else{
+                    let user ={
+                        userId: rows[0].id,
+                        wechatName:rows[0].wechat_name,
+                        wechatId: rows[0].wechat_id,
+                        userStatus: rows[0].status
+                    };
+                    let myDate = new Date();
+                    params.lastLoginOn = myDate;
+                    user.lastLoginOn = params.lastLoginOn;
+                    userDao.lastLoginOn({wechatId:params.wechatId,lastLoginOn:params.lastLoginOn},(error,rows));
+                    user.accessToken = oauthUtil.createAccessToken(oauthUtil.clientType.user,user.userId,user.userStatus);
+                    resUtil.resetQueryRes(res,user,null);
+                    return next();
+                }
             }
         })
     }).then((params)=>{
@@ -171,32 +179,35 @@ const userLogin = (req,res,next)=>{
             }
             userDao.createUser(params,(error,result)=>{
                 if(error){
-                    logger.error('createUser' + error.message);
+                    logger.error('userLogin createUser ' + error.message);
                     resUtil.resInternalError(error, res, next);
                 }
                 else{
                     params.userId = result.insertId;
                     userDao.queryUser({userId:params.userId},(error,rows)=>{
                         if(error){
-                            logger.error('queryUser'+error.message);
+                            logger.error('userLogin createUser queryUser '+error.message);
                             resUtil.resInternalError(error, res, next);
-                        }else if(rows && rows.length < 1){
-                            logger.warn("queryUser"+"创建用户失败");
-                            resUtil.resetFailedRes(res,'创建用户失败',null);
                         }else{
-                            let user ={
-                                userId: rows[0].id,
-                                wechatName:rows[0].wechat_name,
-                                wechatId: rows[0].wechat_id,
-                                userStatus: rows[0].status
-                            };
-                            let myDate = new Date();
-                            params.lastLoginOn = myDate;
-                            user.lastLoginOn = params.lastLoginOn;
-                            userDao.lastLoginOn({wechatId:params.wechatId,lastLoginOn:params.lastLoginOn},(error,rows));
-                            user.accessToken = oauthUtil.createAccessToken(oauthUtil.clientType.user,user.userId,user.userStatus);
-                            resUtil.resetQueryRes(res,user,null);
-                            return next();
+                            logger.info('userLogin createUser queryUser ' + 'success');
+                            if(rows && rows.length < 1){
+                                logger.warn("userLogin createUser queryUser "+"User creation failed.");
+                                resUtil.resetFailedRes(res,'创建用户失败',null);
+                            }else{
+                                let user ={
+                                    userId: rows[0].id,
+                                    wechatName:rows[0].wechat_name,
+                                    wechatId: rows[0].wechat_id,
+                                    userStatus: rows[0].status
+                                };
+                                let myDate = new Date();
+                                params.lastLoginOn = myDate;
+                                user.lastLoginOn = params.lastLoginOn;
+                                userDao.lastLoginOn({wechatId:params.wechatId,lastLoginOn:params.lastLoginOn},(error,rows));
+                                user.accessToken = oauthUtil.createAccessToken(oauthUtil.clientType.user,user.userId,user.userStatus);
+                                resUtil.resetQueryRes(res,user,null);
+                                return next();
+                            }
                         }
                     })
                 }
@@ -214,10 +225,10 @@ const getUserStatByDay = (req,res,next)=>{
     params.dateIdStart = moment(myDateSize).format('YYYYMMDD');
     userDao.getUserStatByDay(params,(error,result)=>{
         if(error){
-            logger.error('getUserStatByDay' + error.message);
+            logger.error('getUserStatByDay ' + error.message);
             resUtil.resInternalError(error, res, next);
         }else{
-            logger.info('getUserStatByDay' + 'success');
+            logger.info('getUserStatByDay ' + 'success');
             resUtil.resetQueryRes(res,result,null);
             return next();
         }
@@ -227,10 +238,10 @@ const getUserStatByMonth = (req,res,next)=>{
     let params = req.params;
     userDao.getUserStatByMonth(params,(error,result)=>{
         if(error){
-            logger.error('getUserStatByMonth' + error.message);
+            logger.error('getUserStatByMonth ' + error.message);
             resUtil.resInternalError(error, res, next);
         }else{
-            logger.info('getUserStatByMonth' + 'success');
+            logger.info('getUserStatByMonth ' + 'success');
             resUtil.resetQueryRes(res,result,null);
             return next();
         }
@@ -240,22 +251,22 @@ const getWXBizDataCrypt = (req,res,next)=>{
     let params = req.params;
     let date = encrypt.WXBizDataCrypt(sysConfig.wechatConfig.mpAppId,params.sessionKey,params.encryptedData,params.iv);
     let myDate = new Date();
-    logger.info("WXBizDataCrypt"+date);
+    logger.info("getWXBizDataCrypt WXBizDataCrypt "+date);
     userDao.queryUser({userId:params.userId},(error,rows)=>{
         if(error){
-            logger.error('queryUser' + error.message);
+            logger.error('getWXBizDataCrypt queryUser ' + error.message);
             resUtil.resInternalError(error, res, next);
         }else if(rows && rows.length < 1){
-            logger.warn('queryUser' + '没有此用户');
+            logger.warn('getWXBizDataCrypt queryUser ' + 'There is no such user.');
             resUtil.resetFailedRes(res,'m查无此用户',null);
         }else{
             if(rows[0].phone && rows[0].phone !== date.purePhoneNumber){
                 userDao.updatePhone({userId:params.userId,phone:date.purePhoneNumber,authStatus:1,myDate:myDate},(error,result)=>{
                     if(error){
-                        logger.error('updatePhone' + error.message);
+                        logger.error('getWXBizDataCrypt updatePhone ' + error.message);
                         resUtil.resInternalError(error, res, next);
                     }else{
-                        logger.info('updatePhone' + 'success');
+                        logger.info('getWXBizDataCrypt updatePhone ' + 'success');
                         resUtil.resetUpdateRes(res,result,null);
                         return next();
                     }
@@ -263,16 +274,16 @@ const getWXBizDataCrypt = (req,res,next)=>{
             }else if(rows[0].phone == null || rows[0].phone == ''){
                 userDao.updatePhone({userId:params.userId,phone:date.purePhoneNumber,authStatus:1,myDate:myDate},(error,result)=>{
                     if(error){
-                        logger.error('queryUser' + error.message);
+                        logger.error('getWXBizDataCrypt updateNullPhone ' + error.message);
                         resUtil.resInternalError(error, res, next);
                     }else{
-                        logger.info('updatePhone' + 'success');
+                        logger.info('getWXBizDataCrypt updateNullPhone ' + 'success');
                         resUtil.resetUpdateRes(res,result,null);
                         return next();
                     }
                 })
             }else{
-                logger.info(rows[0].wechat_name +'此用户是老用户，可以直接登录');
+                logger.info(rows[0].wechat_name +' This user is an old user and can log in directly.');
                 resUtil.resetQueryRes(res,{success:true},null);
                 return next();
             }
@@ -283,10 +294,10 @@ const updateUserImg = (req,res,next)=>{
     let params = req.params;
     userDao.updateUserImg(params,(error,result)=>{
         if(error){
-            logger.error('updateUserImg' + error.message);
+            logger.error('updateUserImg ' + error.message);
             resUtil.resInternalError(error, res, next);
         }else{
-            logger.info('updateUserImg' + 'success');
+            logger.info('updateUserImg ' + 'success');
             resUtil.resetUpdateRes(res,result,null);
             return next();
         }
