f59e2f4206c34f78ad800030fdc40e9e9c781a7a
yingmei567 <yingmei.yan@myxxjs.com>
2019-06-25 11:26:24 +0800

    update return message

--- bl/QrCode.js
+++ bl/QrCode.js
@@ -27,7 +27,7 @@ const getQrCode = (req,res,next)=>{
         //resUtil.resetQueryRes(res,{success:true},null);
 
         logger.warn('getQrCode userType is not Supervise!');
-        resUtil.resetFailedRes(res,'不是交警，无权查询！',null);
+        resUtil.resetFailedRes(res,'No query permissions！',null);
 
     }
     /*oauthUtil.getQrCode({qrCodeId:params.qrCodeId},(error,result)=>{
